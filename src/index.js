0ef81fd9ecf7f45d8f470dc1a1ba9eb15c3d296c
Slava Manzhela <slavamanzh@gmail.com>
2023-01-26 00:01:09 +0300

    feat: add better solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  return true;
+  let s=str.replace('\'', '');
+  const hashMap = { "(": ")", "{": "}", "[": "]" };
+  const stack = [];
+  for (let ch of s) {
+    if (hashMap[ch]) {
+      // ch is an opening bracket
+      stack.push(hashMap[ch]);
+    } else if (stack.length > 0 && stack[stack.length - 1] === ch) {
+      // ch is a closing bracket and top of stack matches
+      stack.pop();
+    } else {
+      // ch is a closing bracket and top of the stack doesn't match
+      return false;
+    }
+  }
+  return stack.length === 0;
 }
